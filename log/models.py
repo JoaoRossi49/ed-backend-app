0c11cff70daa26f3b07f5f2fbf56286fbca7f6e0
JoaoRossi49 <joao.rossi.figueiredo@gmail.com>
2024-06-01 15:47:12 -0300

    feat: criação de aplicação para gerenciamento de logs e nova lógica de autenticação

---
+++ log/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+TIPOS_EVENTO_ACESSO_CHOICES = (
+    ("F", "Falha ao acessar"),
+    ("S", "Sucesso ao acessar")
+)
+
+class Acesso(models.Model):
+    data_inclusao = models.DateTimeField(auto_now_add=True)
+    tipo_evento = models.CharField(max_length=10, choices=TIPOS_EVENTO_ACESSO_CHOICES)
+    user = models.CharField(60)
+    ip = models.CharField(max_length=15)
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    tentativas = models.IntegerField(default=0)
+    bloqueado = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
