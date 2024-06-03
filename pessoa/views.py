fc61d25ad9812b64ece9ab9f7563adb82ab9cc84
JoaoRossi49 <joao.rossi.figueiredo@gmail.com>
2024-06-03 20:36:55 -0300

    feat: atribuição de classe de permissão para endpoints operacionais e implementação de gerador de token em api de login

--- pessoa/views.py
+++ pessoa/views.py
@@ -6,34 +6,44 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from log.models import Acesso, UserProfile
 
 class PessoaList(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Pessoa.objects.all()
     serializer_class = PessoaSerializer
     
 class PessoaUpdate(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Pessoa.objects.all()
     serializer_class = PessoaSerializer 
     
 class EnderecoList(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Endereco.objects.all()
     serializer_class = EnderecoSerializer
 
 class ContatoList(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Contato.objects.all()
     serializer_class = ContatoSerializer   
 
 class DocumentoList(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Documento.objects.all()
     serializer_class = DocumentoSerializer
 
 class RelacaoList(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Relacao.objects.all()
     serializer_class = RelacaoSerializer
 
 class LoginView(APIView):
+    permission_classes = [AllowAny]
     def post(self, request):
         username = request.data.get('username')
         password = request.data.get('password')
@@ -51,11 +61,15 @@ class LoginView(APIView):
         user = authenticate(username=username, password=password)
 
         if user is not None:
+            refresh = RefreshToken.for_user(user)
             user_profile.tentativas = 0
             user_profile.save()
             log = Acesso(user = user, tipo_evento = 'S', ip= request.META.get('REMOTE_ADDR'))
             log.save()
-            return Response(status=status.HTTP_200_OK)
+            return Response({
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            })
         else:
             user_profile.tentativas += 1
             user_profile.save()
