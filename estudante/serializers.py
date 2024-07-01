d69dfce8b72aff44e1c2cafa337663e1b8caf67e
JoaoRossi49 <joao.rossi.figueiredo@gmail.com>
2024-06-30 21:05:01 -0300

    feat: listagem de matrícula agora retorna o nome da turma

--- estudante/serializers.py
+++ estudante/serializers.py
@@ -1,16 +1,25 @@
 from rest_framework import serializers
 from .models import Matricula, Turma
 
-class MatriculaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Matricula
-        fields = '__all__'
-
 class TurmaSerializer(serializers.ModelSerializer):
     data_inicio = serializers.DateField(format="%d/%m/%Y", input_formats=['%d/%m/%Y'])
     class Meta:
         model = Turma
         fields = '__all__'
+
+class MatriculaSerializer(serializers.ModelSerializer):
+    turma_nome = serializers.CharField(source='turma.nome', read_only=True)
+
+    class Meta:
+        model = Matricula
+        fields = '__all__'
+        extra_fields = ['turma_nome']
+
+    def get_field_names(self, declared_fields, info):
+        expanded_fields = super(MatriculaSerializer, self).get_field_names(declared_fields, info)
+        if hasattr(self.Meta, 'extra_fields'):
+            expanded_fields.extend(self.Meta.extra_fields)
+        return expanded_fields
 
 class TurmaCreateSerializer(serializers.ModelSerializer):
     class Meta:
