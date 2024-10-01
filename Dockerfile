808678b397dee7838a123c69a5ed68e94e1393f5
JoaoRossi49 <joao.rossi.figueiredo@gmail.com>
2024-10-01 12:28:05 -0300

    feat: correções no path

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3.11.9
 
 ENV PYTHONBUFFERED=1
 
-ENV PORT 8080
+ENV PORT 8000
 
 WORKDIR /app
 
@@ -12,6 +12,7 @@ RUN pip install --upgrade pip
 
 RUN pip install -r requirements.txt
 
-CMD gunicorn server.wsgi:application --bind 0.0.0.0:"${PORT}"
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000", "--noreload"]
+#CMD gunicorn edmanager.wsgi:application --bind 0.0.0.0:"${PORT}"
 
 EXPOSE ${PORT}
--- edmanager/urls.py
+++ edmanager/urls.py
@@ -9,8 +9,8 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('pessoa.urls')),
-    path('api/estudante/', include('estudante.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include('pessoa.urls')),
+    path('estudante/', include('estudante.urls')),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
