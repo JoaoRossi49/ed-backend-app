d2096b9a57e1ff3c31093deb511cf281a8890c77
JoaoRossi49 <joao.rossi.figueiredo@gmail.com>
2024-10-02 12:55:49 -0300

    feat: migration para contato e endereço não é mais obrigatório

--- pessoa/serializers.py
+++ pessoa/serializers.py
@@ -19,10 +19,10 @@ class DocumentoSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class PessoaSerializer(serializers.ModelSerializer):
-    endereco = EnderecoSerializer()
     contato = ContatoSerializer(many=True)
     documento = DocumentoSerializer(many=True)
-    
+    endereco = EnderecoSerializer(required=False, allow_null=True, default=None)
+
     data_nascimento = serializers.DateField(format="%d/%m/%Y", input_formats=['%d/%m/%Y'])
 
     class Meta:
@@ -30,22 +30,28 @@ class PessoaSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def create(self, validated_data):
-        endereco_data = validated_data.pop('endereco')
+        endereco_data = validated_data.pop('endereco', None)
         contato_data = validated_data.pop('contato')
         documento_data = validated_data.pop('documento')
-        
-        endereco = Endereco.objects.create(**endereco_data)
-        
-        pessoa = Pessoa.objects.create(endereco=endereco, **validated_data)
-        
+
+        pessoa = Pessoa.objects.create(**validated_data)
+
+        if endereco_data:
+            endereco = Endereco.objects.create(**endereco_data)
+            pessoa.endereco = endereco
+            pessoa.save()
+        elif 'endereco' in validated_data and validated_data['endereco'] is None:
+            pessoa.endereco = None
+            pessoa.save()
+
         for contato in contato_data:
             contato_obj = Contato.objects.create(**contato)
             pessoa.contato.add(contato_obj)
-            
+
         for documento in documento_data:
             documento_obj = Documento.objects.create(**documento)
-            pessoa.documento.add(documento_obj)      
-             
+            pessoa.documento.add(documento_obj)
+
         return pessoa
 
     def update(self, instance, validated_data):
@@ -54,9 +60,17 @@ class PessoaSerializer(serializers.ModelSerializer):
         documento_data = validated_data.pop('documento', None)
 
         if endereco_data:
-            for attr, value in endereco_data.items():
-                setattr(instance.endereco, attr, value)
-            instance.endereco.save()
+            if instance.endereco:
+                for attr, value in endereco_data.items():
+                    setattr(instance.endereco, attr, value)
+                instance.endereco.save()
+            else:
+                endereco = Endereco.objects.create(**endereco_data)
+                instance.endereco = endereco
+                instance.save()
+        elif 'endereco' in validated_data and validated_data['endereco'] is None:
+            instance.endereco = None
+            instance.save()
 
         if contato_data:
             instance.contato.clear()
@@ -65,17 +79,17 @@ class PessoaSerializer(serializers.ModelSerializer):
                 instance.contato.add(contato_obj)
 
         if documento_data:
-            instance.documento.clear()        
+            instance.documento.clear()
             for documento in documento_data:
                 documento_obj = Documento.objects.create(**documento)
-                instance.documento.add(documento_obj)           
+                instance.documento.add(documento_obj)
 
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
         instance.save()
-        
-        return instance
 
+        return instance
+    
 class PerfilSerializer(serializers.ModelSerializer):
     class Meta:
         model = Perfil
