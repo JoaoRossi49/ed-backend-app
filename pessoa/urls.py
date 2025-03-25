421a54974930e836d40acacc5f97293ec4f79fcb
joao <joao.rossi.figueiredo@gmail.com>
2025-03-25 20:56:48 -0300

    feat: adequações de rota

--- pessoa/urls.py
+++ pessoa/urls.py
@@ -2,8 +2,8 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('pessoa/', PessoaList.as_view()),
-    path('pessoa/<int:pk>/', PessoaUpdate.as_view()),
+    path('', PessoaList.as_view()),
+    path('<int:pk>/', PessoaUpdate.as_view()),
     path('endereco/',EnderecoList.as_view()),
     path('contato/', ContatoList.as_view()),
     path('login/', LoginView.as_view()),
